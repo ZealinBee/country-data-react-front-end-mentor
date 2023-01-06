95a1ee239cce9f35e4d74b49a8844d3bfdb88cc7
Zhiyuan Liu <thenukezealot@gmail.com>
2023-01-06 12:23:57 +0200

    dark mode toggle

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,29 @@
-import React from 'react'
-import '../styles/nav.scss'
+import React, { useState, useEffect} from "react";
+import "../styles/nav.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBorderStyle, faMoon } from "@fortawesome/free-solid-svg-icons";
 
 function Nav() {
+  const [darkMode, setDarkMode] = useState(true);
+  
+  useEffect(() => {
+    const body = document.querySelector('body')
+    if(darkMode) {
+      body.classList.remove('dark')
+    }else {
+      body.classList.add('dark')
+    }
+  }, [darkMode])
+
   return (
     <nav>
       <h2>Where in the world?</h2>
-      <button>Dark Mode</button>
+
+      <button onClick={()=> setDarkMode(!darkMode)}>
+      <FontAwesomeIcon icon={faMoon} /> <span>Dark Mode</span> 
+      </button>
     </nav>
-  )
+  );
 }
 
-export default Nav
+export default Nav;
